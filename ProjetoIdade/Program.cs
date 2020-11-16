40777746ad5284a7aa762a7cf63e980ae5ab687c
TolentinoGustavo <tolentinogustavo@gmail.com>
2020-11-16 20:49:12 -0300

    Projeto Idade

---
+++ ProjetoIdade/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ProjetoIdade
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Contador de idade");
+            Console.WriteLine("Digite sua idade");
+//       ENTRADA
+                int idade = int.Parse(Console.ReadLine());
+//       PROCESSAMENTO
+                int meses = idade * 12;
+                int dias = idade * 365;
+                int horas = dias * 24;
+                int minutos = horas * 60;
+
+                Console.WriteLine("Voce ja viveu: ");
+                Console.WriteLine("Meses: {0}" , meses);
+                Console.WriteLine("Horas: " + horas);
+                Console.WriteLine("Minutos: " + minutos);
+        }
+    }
+}
